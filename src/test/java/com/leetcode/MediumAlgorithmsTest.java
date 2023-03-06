07a568f092019efa9ce8a96bde2c32dd535171ba
runatalia <voroninana1990@mail.ru>
2023-03-06 23:13:05 +0300

    testAddTwoNumbers()

--- src/test/java/com/leetcode/MediumAlgorithmsTest.java
+++ src/test/java/com/leetcode/MediumAlgorithmsTest.java
@@ -1,6 +1,9 @@
 package com.leetcode;
 
+import additionalClasses.ListNode;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,6 +28,22 @@ public class MediumAlgorithmsTest {
         assertEquals(false, MediumAlgorithms.possibleBipartition(n2, dislikes2));
         assertEquals(false, MediumAlgorithms.possibleBipartition(n3, dislikes3));
         assertEquals(true, MediumAlgorithms.possibleBipartition(n4, dislikes4));
+    }
+
+    public void testAddTwoNumbers() {
+        ListNode l = new ListNode();
+        ListNode node1 = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+        ListNode node2 = new ListNode(2, new ListNode(4, new ListNode(9, new ListNode(5))));
+        ListNode node3 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        ListNode node4 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+        ListNode node5 = new ListNode(0);
+        ListNode node6 = new ListNode(0);
+        ListNode node7 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode node8 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        assertEquals(List.of(0), l.listNodeToArrays(MediumAlgorithms.addTwoNumbers(node1, node2)));
+        assertEquals(List.of(8, 9, 9, 9, 0, 0, 0, 1), l.listNodeToArrays(MediumAlgorithms.addTwoNumbers(node3, node4)));
+        assertEquals(List.of(0), l.listNodeToArrays(MediumAlgorithms.addTwoNumbers(node5, node6)));
+        assertEquals(List.of(7, 0, 8), l.listNodeToArrays(MediumAlgorithms.addTwoNumbers(node7, node8)));
     }
 
     @Test
